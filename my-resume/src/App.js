be1f84344418b16f7212248bdb38836b9a2559cf
karasu yami <demigodlinty@gmail.com>
2023-02-14 13:56:01 -0600

    change css layout and added links to my resume

--- my-resume/src/App.js
+++ my-resume/src/App.js
@@ -1,5 +1,7 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import me from './myFace.jpg';
+import linkedin from './linkedin.png';
+import gitHub from './GitHub.png';
 import './App.css';
 
 function App() {
@@ -7,14 +9,26 @@ function App() {
     <article>
       <header className="App-header">
          Elijah linton
+         <a href='.board'>about me</a>
+         <a> portfolio</a>
+         <a>contact</a>
+        <a>resume</a>   
       </header>
 
       <div className='board'>
         <img className='myPic' alt='a image of a really handsome dude' src={me}></img>
-        <h3>who am i? </h3> 
+          <h3>about me</h3>
+          <p>
+            i am Elijah Linton 20 year old bootcamp student,
+            my skillset consist of html, css, sql, nodejs, express,
+            react js, mongodb, and much more!
+          </p> 
 
      </div>
-     <footer>links</footer>
+     <footer>
+      <a href='https://github.com/ElijahLinton' ><img className="icons" src={gitHub}></img></a>
+      <a href="https://www.linkedin.com/in/elijah-linton-4612b7241/"><img className="icons" src={linkedin} alt="my linkedin page"></img></a>
+     </footer>
     </article>
   );
 }
